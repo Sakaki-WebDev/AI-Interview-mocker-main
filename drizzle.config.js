9e2516a9e2887237a99a22109ebd21652847606b
Sakaki Lab <campbellcasteel@gmail.com>
2024-11-07 22:03:19 +0000

    fixed dbcredentials issue

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,8 @@
 export default {
   schema: './utils/schema.js',
   dialect: 'postgresql',
-  dbCredentials: {
-    url: 'postgresql://ai-interview-mockup_owner:pjS74likNWZz@ep-rapid-field-a5jltqr3.us-east-2.aws.neon.tech/ai-interview-mockup?sslmode=require',
-  },
+  //dbCredentials: {
+  // url: 'postgresql://ai-interview-mockup_owner:pjS74likNWZz@ep-rapid-field-a5jltqr3.us-east-2.aws.neon.tech/ai-interview-mockup?sslmode=require',
+  // },
+  dbCredentials: process.env.URL,
 };
